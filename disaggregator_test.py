d54fa162cb100a53f31bba421e9fe7041aeacd32
uvchik <uvchik.git@posteo.eu>
2019-12-13 13:07:16 +0100

    Add stuff

---
+++ disaggregator_test.py
@@ -0,0 +1,27 @@
+import os
+from disaggregator import config, data
+from reegis import geometries as geo, config as rconfig
+
+nuts_geo_fn = os.path.join(
+    rconfig.get('paths', 'geometry'),
+    'NUTS_RG_03M_2016_4326_LEVL_3_DE.geojson'
+)
+
+nuts_geo = geo.load(fullname=nuts_geo_fn)
+nuts_geo.set_index('id', drop=True, inplace=True)
+fed_states = geo.get_federal_states_polygon()
+
+nuts_geo = geo.spatial_join_with_buffer(nuts_geo.centroid, fed_states, 'fs')
+fed_states['nuts'] = '0'
+for state in fed_states.index:
+    fed_states.loc[state, 'nuts'] = list(nuts_geo.loc[nuts_geo['fs'] == state].index)
+
+cfg = config.get_config()
+dict_nuts3_name = config.region_id_to_nuts3(nuts3_to_name=True)
+df_spatial = data.database_description('spatial')
+df_temporal = data.database_description('temporal')
+elc_consumption_hh_spat = data.elc_consumption_HH_spatial()
+elc_consumption_hh_spattemp = data.elc_consumption_HH_spatiotemporal()
+
+print(elc_consumption_hh_spattemp[fed_states.loc['BB', 'nuts']])
+print(elc_consumption_hh_spattemp[fed_states.loc['HH', 'nuts']])
